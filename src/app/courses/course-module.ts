e1d262906aba52c002e638533f6f1d162d947b16
wdvOps <dvops@outlook.com>
2021-04-16 23:15:45 -0300

    Finalizado

--- src/app/courses/course-module.ts
+++ src/app/courses/course-module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
-import { ReplacePipe } from "../pipe/replace.pipe";
+
 import { CourseInfoComponent } from "./course-info.component";
 import { CourseListComponent } from "./course-list.component";
 import { CommonModule } from "@angular/common"
-import { StarComponent } from "../star/star.component";
+import { StarModule } from "../shared/component/star-module";
+import { AppPipeModule } from "../shared/pipe/app-pipe.module";
+import { CoreModule } from "../core/component/core-module";
+
 
 @NgModule({
     declarations: [
         CourseListComponent,
         CourseInfoComponent,
-        ReplacePipe,
-        StarComponent
+        
+        
     ],
     imports: [
         CommonModule,
         FormsModule,
+        StarModule,
+        AppPipeModule,
+        CoreModule,
         RouterModule.forChild([
             {
               path: 'courses', component: CourseListComponent
